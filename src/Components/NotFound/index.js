b176406d4c1aeed80195e996a2d20a0b995ff49d
Praveeenmain <Praveenlapacc@gmail.com>
2023-12-28 22:36:37 +0530

    updated npm packages

--- src/Components/NotFound/index.js
+++ src/Components/NotFound/index.js
@@ -1,13 +1,24 @@
 import { Component } from "react";
 import Header from "../Header";
-class NotFound extends Component{
-    render(){
-        return(
-            <div>
-                <Header/>
-            <h1> Not Found page</h1>
+import "./index.css"; // Import your CSS file
+import { Link } from "react-router-dom";
+class NotFound extends Component {
+    render() {
+        return (
+           
+            <div className="not-found-container">
+                   <Header />
+                <img
+                    src="https://t4.ftcdn.net/jpg/03/88/63/83/360_F_388638369_wSBADhKfhiTx6Q5Pz1xfdpy6zotku1Sg.jpg"
+                    className="not-found-image"
+                    alt="Not Found"
+                />
+                <Link to="/" className="back-button">
+                    Back to Home
+                </Link>
             </div>
-        )
+        );
     }
 }
-export default NotFound
+
+export default NotFound;
